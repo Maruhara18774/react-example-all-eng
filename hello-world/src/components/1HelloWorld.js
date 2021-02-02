67c918f847a762fcc7f8b0b64c49e38623b2e342
Linh Mac Le My <67138256+Maruhara18774@users.noreply.github.com>
2021-02-02 20:35:04 +0700

    Pure JS for creating elements

--- hello-world/src/components/1HelloWorld.js
+++ hello-world/src/components/1HelloWorld.js
@@ -6,5 +6,11 @@ function HelloWorld(props) {
   return <h3>Hello world</h3>
 }
 
+/*  Pure Javascript 
+const HelloWorld = () => {
+  return React.createElement('div', {id: 'Hello'},
+  React.createElement('h3',{className: 'App-header'},'Hello world'))
+}
+*/
 
 export default HelloWorld;
