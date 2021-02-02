2fe8ada4fcf5f8268d33d295aa9ad7384f9c5575
Linh Mac Le My <67138256+Maruhara18774@users.noreply.github.com>
2021-02-02 20:55:40 +0700

    Props

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -3,18 +3,16 @@ import './App.css';
 import React from 'react';
 import HelloWorld from './components/1HelloWorld';
 import BounjourWorld from './components/2BounjourWorld';
-import {HelloWorldAdv} from './components/3HelloWorldAdv';
+import { HelloWorldAdv } from './components/3HelloWorldAdv';
 import ClickedCount from './components/4Hook';
+import MyName from './components/5Iam'
 
 
 
 function App() {
   return (
     <div className="App">
-      <HelloWorld></HelloWorld>
-      <BounjourWorld></BounjourWorld>
-      <HelloWorldAdv></HelloWorldAdv>
-      <ClickedCount></ClickedCount>
+      <MyName></MyName>
     </div>
   );
 }
---
+++ hello-world/src/components/5Iam.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+/* TEST
+<MyName name = "Emma">
+        <p>Smart boy</p>
+      </MyName>
+*/
+
+class MyName extends Component {
+    constructor(props) {
+        super(props);
+        
+    }
+    render() {
+        return <div>
+            <h3> I am {this.props.name}</h3>
+            <p>My children: {this.props.children}</p>
+        </div>
+
+    }
+}
+
+export default MyName;
