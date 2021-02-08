9958fcb4e8ccf39caa2f33a99c9b5aed2ac82a9e
Linh Mac Le My <67138256+Maruhara18774@users.noreply.github.com>
2021-02-08 21:58:04 +0700

    2/8/2021

--- hello-world/src/components/14Lifecycle.js
+++ hello-world/src/components/14Lifecycle.js
@@ -6,27 +6,106 @@ import React, { Component } from 'react'
     3. componentWillUnmount
     *. componentDidCatch
 */
-class Lifecycle extends Component {
+class Mounting extends Component {
+    // initialize this component
+    constructor(props) {
+        super(props)
+        
+        this.state = {
+             constructor: false,
+             derived: false,
+
+        }
+        //alert("1 - Constructor")
+    }
+    // 
+    static getDerivedStateFromProps(props,state){
+        //alert("2 - Derived")
+    }
+    render() {
+        //alert("3 - Render")
+        return (
+            <div>
+                <form>
+                    <label>Constructor: {}</label>
+                </form>
+            </div>
+        )
+    }
     componentDidMount(){
-        alert("Component did mount.");
+        //alert("4 - Did mount")
     }
-    componentDidUpdate(){
-        alert("Component did update.");
-    }
-    componentWillUnmount(){
-        alert("Component will unmount.")
-    }
-    componentDidCatch(){
-        alert("Component had error.")
-    }
+}
+
+class Updating extends Component {
     render() {
         return (
             <div>
-                <h2>Hey you!</h2>
-                <button>Next</button>
+                
             </div>
         )
     }
 }
+
+class Lifecycle extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             didMount: false,
+             didUpdate: false
+        }
+    }
+    
+    // Runs after the component output has been rendered to the DOM.
+    componentDidMount(){
+        this.state.didMount = true;
+        this.setState(this);
+        // => Update that state will call componentDidUpdate
+        this.state.didUpdate = true;
+        this.setState(this);
+    }
+
+    componentDidUpdate(){
+    }
+
+    // Runs when component is removed
+    componentWillUnmount(){}
+    // Catch error
+    componentDidCatch(){}
+    convertStatus(status){
+        if(status){
+            return "Done";
+        }
+        else{
+            return "Still not complete";
+        }
+    }
+    render() {
+        return (
+            <div>
+                <form>
+                    <div>
+                        <label>Mounting</label>
+                        <p>Status: {this.convertStatus(this.state.didMount)}</p>
+                        <div>
+                        <Mounting></Mounting>
+                        </div>
+                    </div>
+                    <br></br>
+                    <div>
+                        <label>Updating</label>
+                        <p>Status: {this.convertStatus(this.state.didUpdate)}</p>
+                        <div>
+                        <Updating></Updating>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+
 
 export default Lifecycle
